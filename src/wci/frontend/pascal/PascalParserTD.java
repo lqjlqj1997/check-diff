2ac5d9355cb2f2a97310826fd3464e048c1213b0
bla <lqjlqj1997@hotmail.com>
2019-10-10 12:26:27 +0800

    bla

--- src/wci/frontend/pascal/PascalParserTD.java
+++ src/wci/frontend/pascal/PascalParserTD.java
@@ -3,7 +3,9 @@ package wci.frontend.pascal;
 import wci.frontend.*;
 import wci.message.Message;
 
-import static wci.message.MessageType.PARSER_SUMMARY;
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+import static wci.message.MessageType.*;
 
 /**
  * <h1>PascalParserTD</h1>
@@ -15,6 +17,8 @@ import static wci.message.MessageType.PARSER_SUMMARY;
  */
 public class PascalParserTD extends Parser
 {
+    protected static PascalErrorHandler errorHandler = new PascalErrorHandler();
+
     /**
      * Constructor.
      * @param scanner the scanner to be used with this parser.
@@ -34,14 +38,38 @@ public class PascalParserTD extends Parser
         Token token;
         long startTime = System.currentTimeMillis();
 
-        while (!((token = nextToken()) instanceof EofToken)) {}
+        try {
+            // Loop over each token until the end of file.
+            while (!((token = nextToken()) instanceof EofToken)) {
+                TokenType tokenType = token.getType();
 
-        // Send the parser summary message.
-        float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
-        sendMessage(new Message(PARSER_SUMMARY,
-                                new Number[] {token.getLineNumber(),
-                                              getErrorCount(),
-                                              elapsedTime}));
+                if (tokenType != ERROR) {
+
+                    // Format each token.
+                    sendMessage(new Message(TOKEN,
+                                            new Object[] {token.getLineNumber(),
+                                                          token.getPosition(),
+                                                          tokenType,
+                                                          token.getText(),
+                                                          token.getValue()}));
+                }
+                else {
+                    errorHandler.flag(token, (PascalErrorCode) token.getValue(),
+                                      this);
+                }
+
+            }
+
+            // Send the parser summary message.
+            float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
+            sendMessage(new Message(PARSER_SUMMARY,
+                                    new Number[] {token.getLineNumber(),
+                                                  getErrorCount(),
+                                                  elapsedTime}));
+        }
+        catch (java.io.IOException ex) {
+            errorHandler.abortTranslation(IO_ERROR, this);
+        }
     }
 
     /**
@@ -50,6 +78,6 @@ public class PascalParserTD extends Parser
      */
     public int getErrorCount()
     {
-        return 0;
+        return errorHandler.getErrorCount();
     }
 }
--- src/wci/frontend/pascal/PascalScanner.java
+++ src/wci/frontend/pascal/PascalScanner.java
@@ -1,7 +1,11 @@
 package wci.frontend.pascal;
 
 import wci.frontend.*;
+import wci.frontend.pascal.tokens.*;
+
 import static wci.frontend.Source.EOF;
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
 
 /**
  * <h1>PascalScanner</h1>
@@ -30,6 +34,8 @@ public class PascalScanner extends Scanner
     protected Token extractToken()
         throws Exception
     {
+        skipWhiteSpace();
+
         Token token;
         char currentChar = currentChar();
 
@@ -38,10 +44,55 @@ public class PascalScanner extends Scanner
         if (currentChar == EOF) {
             token = new EofToken(source);
         }
+        else if (Character.isLetter(currentChar)) {
+            token = new PascalWordToken(source);
+        }
+        else if (Character.isDigit(currentChar)) {
+            token = new PascalNumberToken(source);
+        }
+        else if (currentChar == '\'') {
+            token = new PascalStringToken(source);
+        }
+        else if (PascalTokenType.SPECIAL_SYMBOLS
+                 .containsKey(Character.toString(currentChar))) {
+            token = new PascalSpecialSymbolToken(source);
+        }
         else {
-            token = new Token(source);
+            token = new PascalErrorToken(source, INVALID_CHARACTER,
+                                         Character.toString(currentChar));
+            nextChar();  // consume character
         }
 
         return token;
+    }
+
+    /**
+     * Skip whitespace characters by consuming them.  A comment is whitespace.
+     * @throws Exception if an error occurred.
+     */
+    private void skipWhiteSpace()
+        throws Exception
+    {
+        char currentChar = currentChar();
+
+        while (Character.isWhitespace(currentChar) || (currentChar == '{')) {
+
+            // Start of a comment?
+            if (currentChar == '{') {
+                do {
+                    currentChar = nextChar();  // consume comment characters
+                } while ((currentChar != '}') && (currentChar != EOF));
+
+                // Found closing '}'?
+                if (currentChar == '}') {
+                    currentChar = nextChar();  // consume the '}'
+                }
+            }
+
+            // Not a comment.
+            else {
+                currentChar = nextChar();  // consume whitespace character
+            }
+        }
     }
 }
